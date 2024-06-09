79b164e2ca9e6d9be59dddff6269d980f0e56555
sebastrog <sebastiancg1070@gmail.com>
2024-06-09 15:55:34 -0500

    add layout heading

---
+++ src/components/layouts/LayoutHeading/LayoutHeading.styles.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+interface HeadingProps {
+  $textAlign: 'center' | 'right';
+}
+
+export const Heading = styled.div<HeadingProps>`
+  margin-bottom: 25px;
+  display: flex;
+  gap: 15px;
+  flex-direction: column;
+  text-align: ${(props) => props.$textAlign || 'left'};
+
+  .title {
+    font-size: 42px;
+    color: ${(props) => props.theme.colors.title};
+  }
+
+  .text {
+    font-size: 18px;
+    color: ${(props) => props.theme.colors.text};
+    opacity: .8;
+  }
+`;
---
+++ src/components/layouts/LayoutHeading/LayoutHeading.tsx
@@ -0,0 +1,16 @@
+import { Heading } from "./LayoutHeading.styles";
+
+interface LayoutHeadingProps {
+  children: React.ReactNode;
+  textAlign: 'center' | 'right';
+}
+
+const LayoutHeading: React.FC<LayoutHeadingProps> = ({children, textAlign }) => {
+  return (
+    <Heading $textAlign={textAlign}>
+      {children}
+    </Heading>
+  )
+}
+
+export default LayoutHeading
