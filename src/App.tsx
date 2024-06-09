02d4993e94fd04661b30ec3ead8f8794fbf25f85
sebastrog <sebastiancg1070@gmail.com>
2024-06-09 13:58:02 -0500

    add initial configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 
+import GlobalStyle from "./styles/GlobalStyles";
+
 import Login from "./pages/Login";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+      <GlobalStyle />
+    </>
   )
 }
 
