f1b109ba756569d9a2ac7f5e1596bd82e3cba0f3
sebastrog <sebastiancg1070@gmail.com>
2024-06-09 16:40:53 -0500

    add context and hook auth

---
+++ src/context/auth/authContext.tsx
@@ -0,0 +1,34 @@
+import { createContext, useState, useEffect } from 'react';
+import { User, onAuthStateChanged } from 'firebase/auth';
+import {auth} from '../../firebase'
+
+interface AuthContextType {
+  user: User | null;
+  loading: boolean;
+}
+
+export const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+interface AuthProviderProps {
+  children: React.ReactNode;
+}
+
+export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
